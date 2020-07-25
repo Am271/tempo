698f6117ccab031c83708df5ca92cd72e5045219
Am271 <44923742+Am271@users.noreply.github.com>
2020-07-25 17:08:10 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -2,11 +2,11 @@
 sudo apt-get install build-essential dkms linux-headers-$(uname -r) android-tools-adb android-tools-fastboot bc bison ca-certificates ccache clang cmake curl file flex gcc g++ git libelf-dev libssl-dev make ninja-build python3 texinfo u-boot-tools zlib1g-dev python vim
 git clone https://github.com/Genom-Project/android_kernel_xiaomi_lavender.git kernel
 cd kernel
-git clone --depth==1 https://github.com/kdrag0n/proton-clang.git clang
+git clone --depth=1 https://github.com/kdrag0n/proton-clang.git clang
 export CONFIG_PATH=$PWD/arch/arm64/configs/lavender-perf_defconfig
 PATH="${PWD}/clang/bin:$PATH"
 export ARCH=arm64
 export KBUILD_BUILD_HOST=circleci
 export KBUILD_BUILD_USER="Am271"
-make O=out ARCH=arm64 lavender-per_defconfig
+make O=out ARCH=arm64 lavender-perf_defconfig
 make -j$(nproc --all) O=out ARCH=arm64 CC=clang CROSS_COMPILE=aarch64-linux-gnu- CROSS_COMPILE_ARM32=arm-linux-gnueabi-
